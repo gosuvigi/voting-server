b986efe53abf8b86137d1c725db566feca6e68b8
gosuvigi <gosuvigi@gmail.com>
2015-09-16 17:12:14 +0200

    - voting moving to the next pair

--- src/core.js
+++ src/core.js
@@ -7,16 +7,55 @@ export function setEntries(state, entries) {
     return state.set('entries', List(entries));
 }
 
+/**
+ * It should create a vote Map on the state, where the two first entries are included under the key pair.
+ * The entries under vote should no longer be in the entries List.
+ *
+ * Once the vote for a given pair is over, we should proceed to the next one.
+ * The winning entry from the current vote should be kept, and added back to the end of the entries,
+ * so that it will later be paired with something else. The losing entry is thrown away.
+ * If there is a tie, both entries are kept.
+ *
+ * @param state
+ * @returns {*}
+ */
 export function next(state) {
-    const entries = state.get('entries');
+    const entries = state.get('entries').concat(getWinners(state.get('vote')));
+
+
     return state.merge({
         vote: Map({pair: entries.take(2)}),
         entries: entries.skip(2)
     });
-
-
 }
 
+function getWinners(vote) {
+    if (!vote) {
+        return [];
+    }
+
+    const [a, b] = vote.get('pair');
+    const aVotes = vote.getIn(['tally', a], 0);
+    const bVotes = vote.getIn(['tally', b], 0);
+
+    if (aVotes > bVotes) {
+        return [a];
+    } else if (aVotes < bVotes) {
+        return [b];
+    } else {
+        return [a, b];
+    }
+}
+
+/**
+ * When a vote is ongoing, it should be possible for people to vote on entries.
+ * When a new vote is cast for an entry, a "tally" for it should appear in the vote.
+ * If there already is a tally for the entry, it should be incremented.
+ *
+ * @param state
+ * @param entry
+ * @returns {*}
+ */
 export function vote(state, entry) {
     return state.updateIn(['vote', 'tally', entry], 0, tally => tally + 1);
 }
--- test/core_spec.js
+++ test/core_spec.js
@@ -14,6 +14,7 @@ describe('application logic', () => {
         it('adds the entries to the state', () => {
             const state = Map();
             const entries = List.of('LotR', 'GoT', 'Matrix');
+
             const nextState = setEntries(state, entries);
 
             expect(nextState).to.equal(Map({entries: List.of('LotR', 'GoT', 'Matrix')}));
@@ -22,21 +23,62 @@ describe('application logic', () => {
         it('converts array to immutable', () => {
             const state = Map();
             const entries = ['LotR', 'GoT', 'Matrix'];
+
             const nextState = setEntries(state, entries);
 
             expect(nextState).to.equal(Map({entries: List.of('LotR', 'GoT', 'Matrix')}));
         });
+
     });
 
     describe('next', () => {
 
         it('takes the next two entries under vote and they should no longer be in the entries list', () => {
             const state = Map({entries: List.of('LotR', 'GoT', 'Matrix')});
+
             const nextState = next(state);
 
             expect(nextState).to.equal(Map({
                 vote: Map({pair: List.of('LotR', 'GoT')}),
                 entries: List.of('Matrix')
+            }));
+        });
+
+        it('puts the winner of current vote back to entries and the should add the next two entries', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('LotR', 'GoT'),
+                    tally: Map({'LotR': 4, 'GoT': 5})
+                }),
+                entries: List.of('Matrix', 'The 13th Floor', 'Dark City')
+            });
+
+            const nestState = next(state);
+
+            expect(nestState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Matrix', 'The 13th Floor'),
+                }),
+                entries: List.of('Dark City', 'GoT')
+            }));
+        });
+
+        it('puts both from tied vote back to entries', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('LotR', 'GoT'),
+                    tally: Map({'LotR': 5, 'GoT': 5})
+                }),
+                entries: List.of('Matrix', 'The 13th Floor', 'Dark City')
+            });
+
+            const nextState = next(state);
+
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Matrix', 'The 13th Floor')
+                }),
+                entries: List.of('Dark City', 'LotR', 'GoT')
             }));
         });
     });
@@ -48,6 +90,7 @@ describe('application logic', () => {
                 vote: Map({pair: List.of('LotR', 'GoT')}),
                 entries: List()
             });
+
             const nextState = vote(state, 'LotR');
 
             expect(nextState).to.equal(Map({
@@ -67,7 +110,9 @@ describe('application logic', () => {
                 }),
                 entries: List()
             });
+
             const nextState = vote(state, 'GoT');
+
             expect(nextState).to.equal(Map({
                 vote: Map({
                     pair: List.of('LotR', 'GoT'),
