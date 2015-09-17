f6549fcd310ddad2354a3b2f6b74e00967b0cd55
gosuvigi <gosuvigi@gmail.com>
2015-09-17 11:33:50 +0200

    - broadcasting state to clients

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@
  */
 import Server from 'socket.io';
 
-export default function startServer() {
+export default function startServer(store) {
     const io = new Server().attach(8090);
+
+    // notify clients when the state has changed
+    store.subscribe(() => io.emit('state', store.getState().toJS()));
+
+    io.on('connection', (socket) => {
+        console.info('--- New client connected, sending the state to the client.');
+        socket.emit('state', store.getState().toJS());
+    });
 }
